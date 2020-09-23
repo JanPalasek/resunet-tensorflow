89be13e444de5cf762d578ee96b0ff56b9f6782b
JanPalasek <jan.palasek@seznam.cz>
2020-09-23 14:44:17 +0200

    Changed to function to bypass tensorflow problem with saving and loading models

--- resunet/model.py
+++ resunet/model.py
@@ -2,75 +2,74 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.layers import *
 
 
-class ResUNet(Model):
-    def __init__(self, input_shape, classes: int, filters_root: int = 64, depth: int = 3):
-        input = Input(shape=input_shape)
+def ResUNet(input_shape, classes: int, filters_root: int = 64, depth: int = 3):
+    input = Input(shape=input_shape)
 
-        layer = input
+    layer = input
 
-        # ENCODER
-        encoder_blocks = []
+    # ENCODER
+    encoder_blocks = []
 
-        filters = filters_root
-        branch = Conv2D(filters=filters, kernel_size=3, strides=1, padding="same", use_bias=False)(layer)
-        branch = BatchNormalization()(branch)
-        branch = ReLU()(branch)
-        branch = Conv2D(filters=filters, kernel_size=3, strides=1, padding="same", use_bias=True)(branch)
-        layer = Add()([branch, layer])
+    filters = filters_root
+    branch = Conv2D(filters=filters, kernel_size=3, strides=1, padding="same", use_bias=False)(layer)
+    branch = BatchNormalization()(branch)
+    branch = ReLU()(branch)
+    branch = Conv2D(filters=filters, kernel_size=3, strides=1, padding="same", use_bias=True)(branch)
+    layer = Add()([branch, layer])
+
+    encoder_blocks.append(layer)
+
+    for _ in range(depth - 1):
+        filters *= 2
+        layer = _down_res_block(layer, filters)
 
         encoder_blocks.append(layer)
 
-        for _ in range(depth - 1):
-            filters *= 2
-            layer = self._down_res_block(layer, filters)
+    # BRIDGE
+    filters *= 2
+    layer = _down_res_block(layer, filters)
 
-            encoder_blocks.append(layer)
+    # DECODER
+    for i in range(1, depth + 1):
+        filters //= 2
+        layer = _up_res_block(layer, encoder_blocks[-i], filters)
 
-        # BRIDGE
-        filters *= 2
-        layer = self._down_res_block(layer, filters)
+    layer = Conv2D(filters=classes, kernel_size=1, strides=1, padding="same")(layer)
+    layer = Softmax()(layer)
 
-        # DECODER
-        for i in range(1, depth + 1):
-            filters //= 2
-            layer = self._up_res_block(layer, encoder_blocks[-i], filters)
+    output = layer
 
-        layer = Conv2D(filters=classes, kernel_size=1, strides=1, padding="same")(layer)
-        layer = Softmax()(layer)
+    return Model(input, output)
 
-        output = layer
+def _down_res_block(layer: Layer, filters: int) -> Layer:
+    branch = BatchNormalization()(layer)
+    branch = ReLU()(branch)
+    branch = Conv2D(filters=filters, kernel_size=3, strides=2, padding="same", use_bias=False)(branch)
 
-        super(ResUNet, self).__init__(input, output)
+    branch = BatchNormalization()(branch)
+    branch = ReLU()(branch)
+    branch = Conv2D(filters=filters, kernel_size=3, strides=1, padding="same", use_bias=False)(branch)
 
-    def _down_res_block(self, layer: Layer, filters: int) -> Layer:
-        branch = BatchNormalization()(layer)
-        branch = ReLU()(branch)
-        branch = Conv2D(filters=filters, kernel_size=3, strides=2, padding="same", use_bias=False)(branch)
+    layer = Conv2D(filters=filters, kernel_size=1, strides=2, padding="same", use_bias=False)(layer)
+    layer = BatchNormalization()(layer)
+    layer = Add()([branch, layer])
 
-        branch = BatchNormalization()(branch)
-        branch = ReLU()(branch)
-        branch = Conv2D(filters=filters, kernel_size=3, strides=1, padding="same", use_bias=False)(branch)
+    return layer
 
-        layer = Conv2D(filters=filters, kernel_size=1, strides=2, padding="same", use_bias=False)(layer)
-        layer = BatchNormalization()(layer)
-        layer = Add()([branch, layer])
+def _up_res_block(layer: Layer, skip_block_connection: Layer, filters: int) -> Layer:
+    layer = UpSampling2D()(layer)
+    layer = Concatenate()([layer, skip_block_connection])
 
-        return layer
+    branch = BatchNormalization()(layer)
+    branch = ReLU()(branch)
+    branch = Conv2D(filters=filters, kernel_size=3, strides=1, padding="same", use_bias=False)(branch)
 
-    def _up_res_block(self, layer: Layer, skip_block_connection: Layer, filters: int) -> Layer:
-        layer = UpSampling2D()(layer)
-        layer = Concatenate()([layer, skip_block_connection])
+    branch = BatchNormalization()(branch)
+    branch = ReLU()(branch)
+    branch = Conv2D(filters=filters, kernel_size=3, strides=1, padding="same", use_bias=False)(branch)
 
-        branch = BatchNormalization()(layer)
-        branch = ReLU()(branch)
-        branch = Conv2D(filters=filters, kernel_size=3, strides=1, padding="same", use_bias=False)(branch)
+    layer = Conv2D(filters=filters, kernel_size=1, strides=1, padding="same", use_bias=False)(layer)
+    layer = BatchNormalization()(layer)
+    layer = Add()([branch, layer])
 
-        branch = BatchNormalization()(branch)
-        branch = ReLU()(branch)
-        branch = Conv2D(filters=filters, kernel_size=3, strides=1, padding="same", use_bias=False)(branch)
-
-        layer = Conv2D(filters=filters, kernel_size=1, strides=1, padding="same", use_bias=False)(layer)
-        layer = BatchNormalization()(layer)
-        layer = Add()([branch, layer])
-
-        return layer
+    return layer
