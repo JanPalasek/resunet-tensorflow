c5ff571b4b9d9e1ec741b1f3b930501fc0018b03
JanPalasek <jan.palasek@seznam.cz>
2021-04-09 10:32:13 +0200

    Added input validation, added tests.

--- resunet/model.py
+++ resunet/model.py
@@ -3,17 +3,24 @@ from tensorflow.keras.layers import *
 
 from resunet.blocks import ResBlock
 
+import math
+
 
 def ResUNet(input_shape, classes: int, filters_root: int = 64, depth: int = 3):
     """
     Builds ResUNet model.
     :param input_shape: Shape of the input images (h, w, c). Note that h and w must be powers of 2.
-    :param classes: Number of classes that will be predicted for each pixel.
+    :param classes: Number of classes that will be predicted for each pixel. Number of classes must be higher than 1.
     :param filters_root: Number of filters in the root block.
-    :param depth: Depth of the architecture.
+    :param depth: Depth of the architecture. Depth must be <= min(log_2(h), log_2(w)).
     :return: Tensorflow model instance.
     """
-    assert classes >= 2, "There has to be prediction for at least 2 classes."
+    if classes < 2:
+        raise ValueError("There has to be prediction for at least 2 classes.")
+    if not math.log(input_shape[0], 2).is_integer() or not math.log(input_shape[1], 2):
+        raise ValueError(f"Input height ({input_shape[0]}) and width ({input_shape[1]}) must be power of two.")
+    if 2 ** depth > min(input_shape[0], input_shape[1]):
+        raise ValueError(f"Model has insufficient height ({input_shape[0]}) and width ({input_shape[1]}) compared to its desired depth ({depth}).")
 
     input = Input(shape=input_shape)
 
--- test_resunet.py
+++ test_resunet.py
@@ -7,15 +7,25 @@ from resunet.model import ResUNet
 
 class ResUNetTests(unittest.TestCase):
     @parameterized.expand([
-        [(128, 128, 1), 4],
-        [(64, 64, 3), 3]
+        [(128, 128, 1), 4, 4, False],
+        [(64, 64, 3), 3, 4, False],
+        [(128, 128, 1), 6, 2, False],
+        [(128, 128, 1), 8, 2, True],
+        [(128, 128, 1), 7, 2, False]
     ])
-    def test_smoke(self, input_shape, depth):
-        filters_root = 4
+    def test_smoke(self, input_shape, depth, filters_root, error=False):
         classes = 2
         inputs = np.ones(shape=input_shape)[np.newaxis, ...]
 
-        model = ResUNet(input_shape, classes, filters_root, depth)
+        try:
+            model = ResUNet(input_shape, classes, filters_root, depth)
+        except Exception:
+            if not error:
+                # if it's not supposed to end as error => raise the error
+                raise
+
+            # otherwise end the test
+            return
         output = model(inputs).numpy().squeeze()
 
         self.assertEqual(output.shape[0], input_shape[0])
