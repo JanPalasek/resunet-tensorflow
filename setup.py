893c8414f9136cd4ae4e72316245ef8d354187af
JanPalasek <jan.palasek@seznam.cz>
2021-04-09 10:32:13 +0200

    Added input validation, added tests.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="resunet",
-    version="0.1a",
+    version="1.0",
     packages=find_packages(),
     install_requires=["tensorflow>=2.0.0"]
 )
