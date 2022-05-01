696c3eb42aa6629c63c71fbf429a832602a3d49e
JanPalasek <jan.palasek@seznam.cz>
2022-05-01 18:12:19 +0200

    RF project structure to avoid some folders being in the package by accident.

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
-from setuptools import setup, find_packages
+import setuptools
 
-setup(
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name="resunet",
     version="1.1a",
-    packages=find_packages(),
+    package_dir={
+        "": "src"
+    },
+    packages=setuptools.find_packages("src"),
     install_requires=["tensorflow>=2.0.0"],
     python_requires=">=3.6",
 )
